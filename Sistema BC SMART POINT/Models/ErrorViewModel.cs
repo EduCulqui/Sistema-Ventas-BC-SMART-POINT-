807785c1755dc3e81c2484544446ed70516a7b9d
eduardo culqui <eduardoculquisan80@gmail.com>
2026-04-02 18:34:23 -0500

    Agregar archivos de proyecto.

---
+++ Sistema BC SMART POINT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistema_BC_SMART_POINT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
